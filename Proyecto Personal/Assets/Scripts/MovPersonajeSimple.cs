9b0e05e8db8cfb9300db1ec738a1136787de5e45
Nahuel Chaves <nahuel_chaves_98@hotmail.com>
2024-07-01 16:57:32 -0300

    feat: Fisicas y sprites idle personaje

--- Proyecto Personal/Assets/Scripts/MovPersonajeSimple.cs
+++ Proyecto Personal/Assets/Scripts/MovPersonajeSimple.cs
@@ -2,31 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moverEslabonScr : MonoBehaviour
-{
+public class moverEslabonScr : MonoBehaviour {
     [Range(0.1f, 10f)]
-    public float speed;
-    [Range(0.1f, 500f)] 
-    public float forceUp;
+    public float Speed;
+    [Range(0.0f, 500f)] 
+    public float ForceUp;
     public float Horizontal;
+    [Range(0.0f, 10f)]
+    public float RayCastJump;
+    public bool Grounded;
 
     Rigidbody2D rb;
     void Start(){
         rb = GetComponent<Rigidbody2D>();
+        Grounded = false;
     }
     void Update() {
         Horizontal = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKeyDown(KeyCode.W)){
+        Debug.DrawRay(transform.position, Vector3.down * RayCastJump, Color.red);
+
+        Grounded = (Physics2D.Raycast(transform.position, Vector3.down, RayCastJump) == true) ? true : false;
+        //Grounded = Physics2D.Raycast(transform.position, Vector3.down, RayCastJump);
+        /*if (Physics2D.Raycast(transform.position, Vector3.down, RayCastJump))
+            Grounded = true;
+        else
+            Grounded = false;*/
+        if (Input.GetKeyDown(KeyCode.W) && Grounded){
             Jump();
         }
 
     }
 
     private void Jump(){
-        rb.AddForce(Vector2.up * forceUp);
+        rb.AddForce(Vector2.up * ForceUp);
     }
     //se usa para fisicas
     private void FixedUpdate(){
-        rb.velocity = new Vector2(Horizontal*speed, rb.velocity.y);
+        rb.velocity = new Vector2(Horizontal*Speed, rb.velocity.y);
     }
 }
